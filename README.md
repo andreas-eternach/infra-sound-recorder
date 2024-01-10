400311814e858d92f8f963798644817165286446
Andreas Eternach <andreas.eternach@gmail.com>
2024-01-10 20:39:46 +0100

    chore: first service version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 sudo apt install i2c-tools
 
+cc i2c_read_test.c -li2c
--- service/infra-service.py
+++ service/infra-service.py
@@ -13,14 +13,14 @@ def signal_handler(sig, frame):
     global process
     process.send_signal(signal.SIGINT)
     time.sleep(1)
+    global logfile
+    logfile.close()
     exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 while keepRunning:
-    with open("timestamp.txt", "a") as f:
-        f.write("The current timestamp is: " + str(datetime.now()))
-        f.close()
     dt_object = datetime.now()
+    global logfile
     logfile = open("/home/pi/i2c/" + 
       format(dt_object.year, '04d') + 
       format(dt_object.month, '02d') + 
@@ -30,8 +30,9 @@ while keepRunning:
       format(dt_object.minute, '02d') + 
       ".csv", "w", 1)
     global process
-    process = subprocess.Popen(["/usr/bin/python3", "/home/pi/i2c/test.py"], stdin = None, stdout = logfile)
-    time.sleep(600)
+    process = subprocess.Popen(["/usr/bin/python3", "/home/pi/i2c/service/test.py"], stdin = None, stdout = logfile)
+    time.sleep(60 * 60 * 6)
     if (keepRunning):
       process.send_signal(signal.SIGINT)
       time.sleep(1)
+      logfile.close()
